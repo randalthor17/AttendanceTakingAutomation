d81260fd8de5aab2d95cc46f5dde0abd703f1d91
randalthor17 <itsmehauhon@gmail.com>
2021-05-22 17:16:37 +0600

    added ways to correct absent list before submitting

--- attendance_message.py
+++ attendance_message.py
@@ -5,10 +5,10 @@ TOTAL = 59
 def main():
     dict = csv_to_dict("Attendence.csv")
     absentee_list = check_absentees(dict)
+    correct_absentee_list(absentee_list)
     absentee_count = len(absentee_list)
     presentee_count = TOTAL - absentee_count
     print_absentee_message(presentee_count, absentee_count)
-    
     
 
 
@@ -38,6 +38,8 @@ def get_date():
     return today
 
 def print_absentee_message(presentee_count, absentee_count):
+    print('This is the message you may copy and paste to Whatsapp Attendance Collection Group.')
+    print('-----------------------------------------------------------------------------------')
     print('@ANK Sir') 
     print('Daily Attendance:')
     print('Date: ' + get_date())
@@ -47,7 +49,46 @@ def print_absentee_message(presentee_count, absentee_count):
     print('Present       : ' + str(presentee_count))
     print('Absent        : ' + str(absentee_count))
     print('Name of FM.   : ANK')
+    print('\n\n')
 
+def correct_absentee_list(list):
+    print('This is the absent list that was found in the .csv file.')
+    print_list(list)
+    print()
+    user_choice = input('Is this correct?(y/n)  ')
+    if user_choice == 'n':
+        user_opt = input('Type a and enter to add absentees, and type r and enter to remove absentees: ')
+        while user_opt != 'f':
+            if user_opt == 'a':
+                user_mod_roll = input('Type in a roll and press enter to add absentee: ')
+                if user_mod_roll in list:
+                    print(user_mod_roll + ' is already in the list.')
+                else:
+                    list.append(user_mod_roll)
+                    print(user_mod_roll + ' was added to the absentee list.')
+                    list.sort()
+                    print('Now the new absentee list stands the following:')
+                    print_list(list)
+            elif user_opt == 'r':
+                user_mod_roll = input('Type in a roll and press enter to remove absentee: ')
+                if user_mod_roll not in list:
+                    print(user_mod_roll + ' is already not in the list.')
+                else:
+                    list.remove(user_mod_roll)
+                    print(user_mod_roll + ' was removed from absentee list.')
+                    print('Now the new absentee list stands the following:')
+                    print_list(list)
+            user_opt = input('Type a and enter to add more absentees, type r and enter to remove more absentees, and type f to finalize absentee list: ')
+
+            
+        
+
+def print_list(list):
+    if len(list) == 0:
+        print('There are no absentees today.')
+    else:
+        for roll in list:
+            print(roll)
 
 if __name__=='__main__':
     main()
