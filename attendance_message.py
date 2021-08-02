f4c0b63e905dcda0624f9b9ac1d0d7302a717c28
randalthor17 <itsmehauhon@gmail.com>
2021-08-02 14:18:23 +0600

    Some formatting and added shebang

--- attendance_message.py
+++ attendance_message.py
@@ -1,7 +1,11 @@
-import csv, datetime, webbrowser
+#!/usr/bin/env python
+import csv
+import datetime
+import webbrowser
 from time import sleep
 
 TOTAL = 59
+
 
 def main():
     dict = csv_to_dict("Attendence.csv")
@@ -14,7 +18,6 @@ def main():
     print_list(absentee_list)
     input("When you're done submitting the contacts, just hit enter.")
     create_url(absentee_list, absentee_count, presentee_count)
-    
 
 
 def csv_to_dict(attendance_file):
@@ -36,22 +39,24 @@ def check_absentees(dict):
             list.append(key)
     return list
 
-    
+
 def get_date():
     today = datetime.date.today()
     today = today.strftime("%d-%m-%Y")
     return today
+
 
 def get_date_weekday():
     today = datetime.date.today()
     today = today.strftime("%d-%m-%Y+%A")
     return today
 
+
 def print_absentee_message(presentee_count, absentee_count):
     print('This is the message you may copy and paste to Whatsapp Attendance Collection Group.')
     print('-----------------------------------------------------------------------------------')
     print()
-    print('@ANK Sir') 
+    print('@ANK Sir')
     print('Daily Attendance:')
     print('Date: ' + get_date())
     print('Form & Section : ')
@@ -63,7 +68,7 @@ def print_absentee_message(presentee_count, absentee_count):
     print('\n')
     input("When you're done submitting the contacts, just hit enter.")
     print()
-    
+
 
 def correct_absentee_list(list):
     print('This is the absent list that was found in the .csv file.')
@@ -71,10 +76,12 @@ def correct_absentee_list(list):
     print()
     user_choice = input('Is this correct?(y/n)  ')
     if user_choice == 'n':
-        user_opt = input('Type a and enter to add absentees, and type r and enter to remove absentees: ')
+        user_opt = input(
+            'Type a and enter to add absentees, and type r and enter to remove absentees: ')
         while user_opt != 'f':
             if user_opt == 'a':
-                user_mod_roll = input('Type in a roll and press enter to add absentee: ')
+                user_mod_roll = input(
+                    'Type in a roll and press enter to add absentee: ')
                 if user_mod_roll in list:
                     print(user_mod_roll + ' is already in the list.')
                 else:
@@ -84,7 +91,8 @@ def correct_absentee_list(list):
                     print('Now the new absentee list stands the following:')
                     print_list(list)
             elif user_opt == 'r':
-                user_mod_roll = input('Type in a roll and press enter to remove absentee: ')
+                user_mod_roll = input(
+                    'Type in a roll and press enter to remove absentee: ')
                 if user_mod_roll not in list:
                     print(user_mod_roll + ' is already not in the list.')
                 else:
@@ -92,10 +100,11 @@ def correct_absentee_list(list):
                     print(user_mod_roll + ' was removed from absentee list.')
                     print('Now the new absentee list stands the following:')
                     print_list(list)
-            user_opt = input('Type a and enter to add more absentees, type r and enter to remove more absentees, and type f to finalize absentee list: ')
+            user_opt = input(
+                'Type a and enter to add more absentees, type r and enter to remove more absentees, and type f to finalize absentee list: ')
     print('\n')
 
-            
+
 def print_list(list):
     if len(list) == 0:
         print('There are no absentees today.')
@@ -103,13 +112,15 @@ def print_list(list):
         for roll in list:
             print(roll)
 
+
 def create_url(absentee_list, absentee_count, presentee_count):
     with open('url_format.txt', 'r') as txt:
         url_src = txt.read().splitlines()
         student_list = csv_to_dict('student_list.csv')
-        with open('url.txt', mode = 'a', newline = '') as url_file:
+        with open('url.txt', mode='a', newline='') as url_file:
             url_file.write(url_src[0])
-            submitter = input('Write your nickname and roll (like this: Auhon+5717 with no spaces, if kept blank, the default is Auhon+5717): ')
+            submitter = input(
+                'Write your nickname and roll (like this: Auhon+5717 with no spaces, if kept blank, the default is Auhon+5717): ')
             if submitter == '':
                 submitter = 'Auhon+5717'
             url_file.write('&' + url_src[1] + '=' + submitter)
@@ -122,7 +133,8 @@ def create_url(absentee_list, absentee_count, presentee_count):
                     url_file.write('&' + url_src[6] + '=' + student_list[roll])
             if absentee_count == 0:
                 url_file.write('&' + url_src[6] + '=None')
-            comment = input('Enter Comment (Default is: Nothing unusual today.): ')
+            comment = input(
+                'Enter Comment (Default is: Nothing unusual today.): ')
             if comment == '':
                 comment = 'Nothing unusual today.'
             comment = comment.replace(' ', '+')
@@ -130,7 +142,6 @@ def create_url(absentee_list, absentee_count, presentee_count):
     print('Now we will open your browser and show you the pre-filled Google forms. Just submit it.')
     sleep(1)
     open_url('url.txt')
-    
 
 
 def open_url(url_file):
@@ -141,6 +152,5 @@ def open_url(url_file):
     url_file.close()
 
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
